b1d73cd910bd3b7561709aa10471e3432e98addd
DQbryant <45484155+DQbryant@users.noreply.github.com>
2019-03-04 23:37:24 +0800

    Add files via upload

---
+++ Conn.java
@@ -0,0 +1,18 @@
+import java.sql.*;
+public class Conn {
+    Connection con;
+    public Connection getConnection(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        try {
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Novels?serverTimezone=UTC","root","ldq!0084");
+            System.out.println("数据库连接成功");
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return con;
+    }
+}
